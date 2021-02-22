a2d87ee79e2e1fabb5f81f31d1500bef6a1aa416
Nicholas Bryant <nbryant.developer@gmail.com>
2021-02-22 00:43:34 -0500

    feat(header): added header
    - added header
    - added svg support
    - added tests
    
    relates header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <nav id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <font-awesome-icon
-        icon="user-secret"
-        :style="{ color: 'var(--forest-light)' }"
-      />
-    </nav>
+    <AppHeader />
     <router-view />
   </div>
 </template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -19,19 +22,5 @@
   color: $swamp-light;
   background-color: $swamp-ultra-dark;
   overflow: hidden;
-}
-
-#nav {
-  padding: 30px;
-  height: $header-height;
-
-  a {
-    font-weight: bold;
-    color: $forest-light;
-  }
-}
-
-#nav a.router-link-exact-active {
-  color: $forest-dark;
 }
 </style>
